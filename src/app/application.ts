de032db37ed847a5acc4023392d2b4bf49b0551c
Fedor Pichugin <pf2772@gmail.com>
2025-05-12 14:35:07 +0500

    remoed old logger

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,10 +1,10 @@
 import { injectable } from 'inversify';
-import logger from '../shared/libs/logger.js';
-import config from '../config/index.js';
+// import logger from '../shared/libs/logger.js';
+// import config from '../config/index.js';
 
 @injectable()
 export class Application {
   public init(): void {
-    logger.info(`application initialized on port ${config.port}`);
+    // logger.info(`application initialized on port ${config.port}`);
   }
 }

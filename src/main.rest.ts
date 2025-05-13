802ed7136cebaf80d2d48712ee9d815810a4a230
Fedor <pichuginf2016@mail.ru>
2025-05-13 13:51:42 +0500

    adds database connection

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
-import { Logger, PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
-import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
 import { Component } from './shared/types/index.js';
+import { Logger, PinoLogger } from './shared/libs/logger/index.js';
+import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
+import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
 
 
 async function bootstrap() {
@@ -11,6 +12,7 @@ async function bootstrap() {
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
   container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
+  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
 
   const application = container.get<RestApplication>(Component.RestApplication);
   await application.init();
